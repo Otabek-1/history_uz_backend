b73ec18ad5ed3fe4af4964d348b8840e676a7264
Otabek <burhonovotabek5@gmail.com>
2025-02-04 20:54:00 +0500

    added

--- Models/testModel.js
+++ Models/testModel.js
@@ -8,7 +8,7 @@ const createTestSession = async (title, description, authorId, startsAt, endsAt,
         
         const result = await pool.query(
             `INSERT INTO test_sessions (title, description, participants, starts_at, ends_at, tests, results)
-             VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+             VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
             [
               title, 
               description, 
