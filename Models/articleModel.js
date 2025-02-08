7cf8c83116fa1a5b9e7b274bdfb15d4970b7e4b6
Otabek <burhonovotabek5@gmail.com>
2025-02-09 03:33:37 +0500

    added

--- Models/articleModel.js
+++ Models/articleModel.js
@@ -64,14 +64,16 @@ const createArticle = async (title, content, authorId, image) => {
   const slug = slugify(title, { lower: true });
   try {
     const result = await pool.query(
-      'INSERT INTO articles (title, content, slug, author_id, image, comments) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-      [title, content, slug, authorId, image, JSON.stringify([])]
+      `INSERT INTO articles (title, content, slug, author_id, image, comments) 
+       VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+      [title, content, slug, authorId, image, JSON.stringify([])] // JSONB uchun to‘g‘ri format
     );
     return result.rows[0];
   } catch (error) {
     throw new Error(error.message);
   }
 };
+
 
 // Maqolani yangilash
 const updateArticle = async (id, title, content, image) => {
